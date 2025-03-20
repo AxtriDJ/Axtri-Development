2790f025a6ae82275799a517c84ab650b975a32b
Jitendra kumar <jitendrakumar8080397415@gmail.com>
2025-03-20 17:38:55 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ function App() {
   return (
     <>
       <div className="Axtri_main">
-        <div className="par">
-          <h3>Welcome to Axtri Software Services , Our Website is under development</h3>
-          <h3>Meanwhile you can see the insights of Our compnay</h3>
-        </div>
         <iframe
           src="https://embed.figma.com/proto/RjrvbpcAIc9u3ghLKPvcFc/Axtri?node-id=679-9207&t=XWSvPAaSrZhPISPk-0&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&hide-ui=1&embed-host=figma-embed-docs"
           frameborder="0"
-          className="iframe_design" 
+          className="iframe_design"
+          title="iframe design"
           allowfullscreen
         >
 
